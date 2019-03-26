40b52e096c2e79251b47996d9c070cff5fbacb11
Alaa Ayyash <aja5063@gmail.com>
2019-03-26 19:24:17 -0400

    - create add_task function to __init__.py    -  adds task item    -  adds timestamp
    - create list.html
       -  contains form for user input
       -  contains submit button
       -  added unordered list of task with timestamp
    
    - add CSS to form in list.html

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flask-todo',
+    name='flask_todo',
     version='0.0.0',
     packages=find_packages(),
     include_package_data=True,
